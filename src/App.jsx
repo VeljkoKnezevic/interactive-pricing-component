f75afead1c8737341d547fdd1d7009df898df360
Veljko <veljkoBitno@gmail.com>
2023-03-23 13:15:25 +0100

    Header styling done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import "./styles/styles.scss";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
+import Switch from "react-switch";
 import { useState } from "react";
 
 function App() {
   const [pageviews, setPageviews] = useState(100000);
   const [pricing, setPricing] = useState(16);
+  const [toggle, setToggle] = useState(false);
+
   return (
     <div className="App">
-      <header>
+      <header className="header">
         <h1 className="header__heading">Simple, traffic-based pricing</h1>
         <p className="header__para">
           Sign-up for our 30-day trial. No credit card required.
@@ -16,22 +19,28 @@ function App() {
       </header>
       <main>
         <div className="container">
-          <p className="container__views">{pageviews} Pageviews</p>
-          <Slider />
+          <p className="container__pageviews">{pageviews} Pageviews</p>
+          <Slider className="container__slider slider" />
           <p className="container__price">
-            <span>${pricing}</span>/month
+            <span>${pricing.toFixed(2)}</span>/ month
           </p>
           <div className="container__billing">
             <p className="container__billing__text">Monthly Billing</p>
-            <button
-              aria-label="billing toggle"
-              className="container__billing__toggle"
-              type="button"
-            >
-              <div className="container__billing__toggle--dot" />
-            </button>
+            <Switch
+              className="container__billing__switch"
+              onColor="#7AEADF"
+              offColor="#CFD8EF"
+              offHandleColor="#FFFFFF"
+              onHandleColor="#FFFFFF"
+              uncheckedIcon={false}
+              checkedIcon={false}
+              checked={toggle}
+              onChange={() => setToggle(!toggle)}
+              height={22}
+              width={43}
+            />
             <p className="container__billing__text">Yearly Billing</p>
-            <p className="container__billing__discount">25% discount</p>
+            <p className="container__billing__discount">25%</p>
           </div>
           <ul className="container__list">
             <li className="container__list__item">Unlimited websites</li>
